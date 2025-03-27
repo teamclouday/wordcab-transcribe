83a69b12e61ad7d6e618f98d6c1896d9d6b3d587
Sida <sida@kulamind.com>
2025-03-26 20:47:52 -0400

    fix url endpoint

--- app/router/v1/audio_url_endpoint.py
+++ app/router/v1/audio_url_endpoint.py
@@ -48,73 +48,43 @@ async def inference_with_audio_url(
     background_tasks: BackgroundTasks,
     url: str,
     data: AudioRequest | None = None,
-) -> dict:
+) -> AudioResponse:
     """Inference endpoint with audio url."""
     filename = f"audio_url_{shortuuid.ShortUUID().random(length=32)}"
     data = AudioRequest() if data is None else AudioRequest(**data.model_dump())
+    try:
+        async with download_limit:
+            _filepath = await download_audio_file("url", url, filename)
 
-    async def process_audio(data: AudioRequest) -> None:
-        try:
-            async with download_limit:
-                _filepath = await download_audio_file("url", url, filename)
+            num_channels = await check_num_channels(_filepath)
+            if (num_channels > 1 and data.multi_channel is False) or (num_channels == 1 and data.multi_channel is True):
+                num_channels = 1  # Force mono channel if more than 1 channel or vice versa
+                new_data = data.model_dump()
+                if data.multi_channel:
+                    new_data["diarization"] = True
+                new_data["multi_channel"] = False
+                data = AudioRequest(**new_data)
 
-                num_channels = await check_num_channels(_filepath)
-                if (num_channels > 1 and data.multi_channel is False) or (
-                    num_channels == 1 and data.multi_channel is True
-                ):
-                    num_channels = 1  # Force mono channel if more than 1 channel or vice versa
-                    new_data = data.model_dump()
-                    if data.multi_channel:
-                        new_data["diarization"] = True
-                    new_data["multi_channel"] = False
-                    data = AudioRequest(**new_data)
-
+            try:
+                filepath: str | list[str] = await process_audio_file(_filepath, num_channels=num_channels)
+            except Exception as e:
                 try:
-                    filepath: str | list[str] = await process_audio_file(_filepath, num_channels=num_channels)
-                except Exception as e:
-                    try:
-                        background_tasks.add_task(delete_file, filepath=filename)
-                        background_tasks.add_task(delete_file, filepath=filepath)
-                    except Exception:
-                        logger.debug("Failed to delete files")
-                    raise HTTPException(  # noqa: B904
-                        status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR,
-                        detail=f"Process failed: {e}",
-                    )
-
-                background_tasks.add_task(delete_file, filepath=filename)
-
-                task = asyncio.create_task(
-                    asr.process_input(
-                        filepath=filepath,
-                        url=url,
-                        url_type="url",
-                        offset_start=data.offset_start,
-                        offset_end=data.offset_end,
-                        num_speakers=data.num_speakers,
-                        diarization=data.diarization,
-                        batch_size=data.batch_size,
-                        multi_channel=data.multi_channel,
-                        source_lang=data.source_lang,
-                        num_beams=data.num_beams,
-                        timestamps_format=data.timestamps,
-                        vocab=data.vocab,
-                        word_timestamps=data.word_timestamps,
-                        internal_vad=data.internal_vad,
-                        repetition_penalty=data.repetition_penalty,
-                        compression_ratio_threshold=data.compression_ratio_threshold,
-                        log_prob_threshold=data.log_prob_threshold,
-                        no_speech_threshold=data.no_speech_threshold,
-                        condition_on_previous_text=data.condition_on_previous_text,
-                    ),
+                    background_tasks.add_task(delete_file, filepath=filename)
+                    background_tasks.add_task(delete_file, filepath=filepath)
+                except Exception:
+                    logger.debug("Failed to delete files")
+                raise HTTPException(  # noqa: B904
+                    status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Process failed: {e}",
                 )
 
-                result = await task
+            background_tasks.add_task(delete_file, filepath=filename)
 
-                utterances, process_times, audio_duration = result
-                result = AudioResponse(
-                    utterances=utterances,
-                    audio_duration=audio_duration,
+            task = asyncio.create_task(
+                asr.process_input(
+                    filepath=filepath,
+                    url=url,
+                    url_type="url",
                     offset_start=data.offset_start,
                     offset_end=data.offset_end,
                     num_speakers=data.num_speakers,
@@ -122,7 +92,8 @@ async def inference_with_audio_url(
                     batch_size=data.batch_size,
                     multi_channel=data.multi_channel,
                     source_lang=data.source_lang,
-                    timestamps=data.timestamps,
+                    num_beams=data.num_beams,
+                    timestamps_format=data.timestamps,
                     vocab=data.vocab,
                     word_timestamps=data.word_timestamps,
                     internal_vad=data.internal_vad,
@@ -131,28 +102,50 @@ async def inference_with_audio_url(
                     log_prob_threshold=data.log_prob_threshold,
                     no_speech_threshold=data.no_speech_threshold,
                     condition_on_previous_text=data.condition_on_previous_text,
-                    job_name=data.job_name,
-                    task_token=data.task_token,
-                    process_times=process_times,
-                )
+                ),
+            )
 
-                if settings.debug:
-                    logger.debug(f"Result: {result.model_dump()}")
+            result = await task
 
-                background_tasks.add_task(delete_file, filepath=filepath)
-        except Exception as e:
-            try:
-                background_tasks.add_task(delete_file, filepath=filename)
-                background_tasks.add_task(delete_file, filepath=filepath)
-            except Exception:
-                logger.debug("Failed to delete files")
-            error_message = f"Error during transcription: {e}"
-            with contextlib.suppress(Exception):
-                logger.error(result.message)
-            logger.error(error_message)
+            utterances, process_times, audio_duration = result
+            result = AudioResponse(
+                utterances=utterances,
+                audio_duration=audio_duration,
+                offset_start=data.offset_start,
+                offset_end=data.offset_end,
+                num_speakers=data.num_speakers,
+                diarization=data.diarization,
+                batch_size=data.batch_size,
+                multi_channel=data.multi_channel,
+                source_lang=data.source_lang,
+                timestamps=data.timestamps,
+                vocab=data.vocab,
+                word_timestamps=data.word_timestamps,
+                internal_vad=data.internal_vad,
+                repetition_penalty=data.repetition_penalty,
+                compression_ratio_threshold=data.compression_ratio_threshold,
+                log_prob_threshold=data.log_prob_threshold,
+                no_speech_threshold=data.no_speech_threshold,
+                condition_on_previous_text=data.condition_on_previous_text,
+                job_name=data.job_name,
+                task_token=data.task_token,
+                process_times=process_times,
+            )
 
-    # Add the process_audio function to background tasks
-    background_tasks.add_task(process_audio, data)
+            if settings.debug:
+                logger.debug(f"Result: {result.model_dump()}")
 
-    # Return the job name and task token immediately
-    return {"job_name": data.job_name, "task_token": data.task_token}
+            background_tasks.add_task(delete_file, filepath=filepath)
+    except Exception as e:
+        try:
+            background_tasks.add_task(delete_file, filepath=filename)
+            background_tasks.add_task(delete_file, filepath=filepath)
+        except Exception:
+            logger.debug("Failed to delete files")
+        error_message = f"Error during transcription: {e}"
+        with contextlib.suppress(Exception):
+            logger.error(result.message)
+        logger.error(error_message)
+        raise
+    else:
+        return result
